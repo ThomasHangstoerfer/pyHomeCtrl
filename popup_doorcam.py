a037ae55c51fb4706612578480fd8d8e865f3a6c
Thomas Hangstörfer <thomas@bitcoder.de>
2025-01-26 16:45:20 +0100

    Switch to camera live stream

--- popup_doorcam.py
+++ popup_doorcam.py
@@ -10,6 +10,56 @@ from display_ctrl import DisplayControl
 from utils import RepeatedTimer
 
 
+from kivy.app import App
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+import cv2
+
+
+class KivyCamera(Image):
+
+    def __init__(self, capture, fps, **kwargs):
+        super(KivyCamera, self).__init__(**kwargs)
+        self.capture = capture
+        self.cam_update_locked = False
+
+        print("fps: ", (1 / fps))
+        Clock.schedule_interval(self.update, 1 / fps)
+
+    def set_capture(self, capture):
+        self.capture = capture
+
+    def activate(self):
+        pass
+
+    def deactivate(self):
+        pass
+
+    def update(self, dt):
+        if self.capture is None:
+            print("KivyCamera.update() capture is None")
+            return
+        print("KivyCamera.update() capture.isOpened()=", self.capture.isOpened(), "cam_update_locked =", self.cam_update_locked)
+        if self.cam_update_locked:
+            return
+        self.cam_update_locked = True
+        ret, frame = self.capture.read()
+        if ret:
+            # convert it to texture
+            buf1 = cv2.flip(frame, 0)
+            buf = buf1.tostring()
+            image_texture = Texture.create(
+                size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            #print("saving")
+            #image_texture.save("file.png")
+            # display image from the texture
+            self.texture = image_texture
+        self.cam_update_locked = False
+
+
+
 class DoorCamPopup(Popup):
     old_display_status = False
 
@@ -28,11 +78,20 @@ class DoorCamPopup(Popup):
         """
 
         self.content = FloatLayout()
-        self.image = Image( size_hint=(1.0, 1.0))
-        self.content.add_widget(self.image)
+
+        #self.image = Image( size_hint=(1.0, 1.0))
+        #self.content.add_widget(self.image)
+
+        #self.camera = KivyCamera(capture=None, size_hint=(1.0, 1.0), fps=1.0)
+        #self.camera = KivyCamera(capture=None, size_hint=(1.0, 1.0), fps=500.0)
+        self.camera = KivyCamera(capture=None, size_hint=(1.0, 1.0), fps=5.0)
+        self.content.add_widget(self.camera)
+
         self.button = Button(text='X', size_hint=(0.07, 0.1), pos_hint={'x': .9, 'y': .9})
         self.button.bind(on_press=self.dismiss)
         self.content.add_widget(self.button)
+
+        self.capture = None
 
         print('DoorCamPopup()__init__ end')
 
@@ -44,7 +103,23 @@ class DoorCamPopup(Popup):
             del self.dismiss_timer
             self.dismiss_timer = None
         self.dismiss_timer = RepeatedTimer(10, self.dismiss_popup, "DoorCamPopup.on_open")
-        pass
+
+
+        self.camera.activate()
+
+        print('DooCamPopup: on_open() creating VideoCapture')
+        self.cam_user = 'admin'
+        self.cam_password = 'GEHEIM'
+        self.cam_host = '192.168.1.177'
+        self.cam_stream = 'Preview_01_sub'
+        #self.cam_stream = 'Preview_01_main'
+        self.capture = cv2.VideoCapture('rtsp://'+self.cam_user+':'+self.cam_password+'@'+self.cam_host+'/'+self.cam_stream)
+        print('DooCamPopup: on_open() VideoCapture created')
+        print('DooCamPopup: on_open() VideoCapture frame size = ', self.capture.get(cv2.CAP_PROP_FRAME_WIDTH), "x", self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        self.camera.set_capture(self.capture)
+        #self.my_camera = KivyCamera(capture=self.capture, fps=500.0)
+
 
     def on_dismiss(self):
         print('on_dismiss')
@@ -53,7 +128,12 @@ class DoorCamPopup(Popup):
             del self.dismiss_timer
         self.dismiss_timer = None
         DisplayControl().unlock()
-        pass
+
+        self.camera.deactivate()
+
+        if self.capture is not None:
+            self.capture.release()
+            self.capture = None
 
     def set_image_filename(self, filename):
         print('DoorCamPopup.set_image_filename(): ', filename)
@@ -62,7 +142,7 @@ class DoorCamPopup(Popup):
             del self.dismiss_timer
             #self.dismiss_timer.restart()
         self.dismiss_timer = RepeatedTimer(10, self.dismiss_popup, "DoorCamPopup.set_image_filename")
-        self.image.source = filename
+        #self.image.source = filename
         #self.image.reload()
 
     def dismiss_popup(self, arg):
